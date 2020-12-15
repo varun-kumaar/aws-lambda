e019fa067a7782e41f619e82adb2029c03859f91
Varun Kumaar <72726414+varun-kumaar@users.noreply.github.com>
2020-12-15 23:11:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello Github!'),
+        body: JSON.stringify('Hola Github!'),
     };
     return response;
 };
